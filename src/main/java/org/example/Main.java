192876ee61b3ccff430c24ea2e3e340e74a0d002
Ravindupereraofficial <ravindurandika2004@gmail.com>
2024-12-22 15:45:24 +0530

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -5,16 +7,14 @@ import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main extends Application {
+public class Main extends Application{
     public static void main(String[] args) {
         launch();
-
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/samplenew.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/HomePage.fxml"))));
         primaryStage.show();
     }
 }
